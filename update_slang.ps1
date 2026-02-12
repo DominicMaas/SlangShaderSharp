41ffa1a9da67566c0d0174a7891059d47c3bed72
Dominic Maas <dominic.maas@live.com>
2026-02-13 11:16:38 +1300

    Lets try running the tests correctly

--- update_slang.ps1
+++ update_slang.ps1
@@ -20,23 +20,12 @@ Expand-Archive -Path "$env:TEMP\slang\linux-x86_64.zip"     -DestinationPath "$e
 # Copy binaries to destination folders
 Copy-Item -Path "$env:TEMP\slang\windows-aarch64\bin\slang-compiler.dll"                  -Destination ".\runtimes\win-arm64\native\slang-compiler.dll"        -Force
 Copy-Item -Path "$env:TEMP\slang\windows-x86_64\bin\slang-compiler.dll"                   -Destination ".\runtimes\win-x64\native\slang-compiler.dll"          -Force
-
-Copy-Item -Path "$env:TEMP\slang\macos-x86_64\lib\libslang-compiler.0.$($version).dylib"  -Destination ".\runtimes\osx-x64\native\libslang-compiler.0.$($version).dylib"     -Force
-Copy-Item -Path "$env:TEMP\slang\macos-x86_64\lib\libslang-compiler.dylib"                -Destination ".\runtimes\osx-x64\native\libslang-compiler.dylib"     -Force
-
-
-Copy-Item -Path "$env:TEMP\slang\macos-aarch64\lib\libslang-compiler.0.$($version).dylib" -Destination ".\runtimes\osx-arm64\native\libslang-compiler.0.$($version).dylib"   -Force
-Copy-Item -Path "$env:TEMP\slang\macos-aarch64\lib\libslang-compiler.dylib"               -Destination ".\runtimes\osx-arm64\native\libslang-compiler.dylib"   -Force
-
-
-Copy-Item -Path "$env:TEMP\slang\linux-aarch64\lib\libslang-compiler.so.0.$($version)"    -Destination ".\runtimes\linux-arm64\native\libslang-compiler.so.0.$($version)"    -Force
-Copy-Item -Path "$env:TEMP\slang\linux-aarch64\lib\libslang-compiler.so"                  -Destination ".\runtimes\linux-arm64\native\libslang-compiler.so"    -Force
-
-
-Copy-Item -Path "$env:TEMP\slang\linux-x86_64\lib\libslang-compiler.so.0.$($version)"     -Destination ".\runtimes\linux-x64\native\libslang-compiler.so.0.$($version)"      -Force
-Copy-Item -Path "$env:TEMP\slang\linux-x86_64\lib\libslang-compiler.so"                   -Destination ".\runtimes\linux-x64\native\libslang-compiler.so"      -Force
+Copy-Item -Path "$env:TEMP\slang\macos-x86_64\lib\libslang-compiler.0.$($version).dylib"  -Destination ".\runtimes\osx-x64\native\libslang-compiler.dylib"     -Force
+Copy-Item -Path "$env:TEMP\slang\macos-aarch64\lib\libslang-compiler.0.$($version).dylib" -Destination ".\runtimes\osx-arm64\native\libslang-compiler.dylib"   -Force
+Copy-Item -Path "$env:TEMP\slang\linux-aarch64\lib\libslang-compiler.so.0.$($version)"    -Destination ".\runtimes\linux-arm64\native\libslang-compiler.so"    -Force
+Copy-Item -Path "$env:TEMP\slang\linux-x86_64\lib\libslang-compiler.so.0.$($version)"     -Destination ".\runtimes\linux-x64\native\libslang-compiler.so"      -Force
 
 # Copy Header Files
-Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang.h"                          -Destination ".\headers\slang.h"                     -Force
-Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang-deprecated.h"               -Destination ".\headers\slang-deprecated.h"          -Force
-Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang-image-format-defs.h"        -Destination ".\headers\slang-image-format-defs.h"   -Force
+Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang.h"                      -Destination ".\headers\slang.h"                     -Force
+Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang-deprecated.h"           -Destination ".\headers\slang-deprecated.h"          -Force
+Copy-Item -Path "$env:TEMP\slang\windows-x86_64\include\slang-image-format-defs.h"    -Destination ".\headers\slang-image-format-defs.h"   -Force
