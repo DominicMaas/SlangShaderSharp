b3696ea627954e033a6e24fab51a06bee0b5bd40
Dominic Maas <dominic.maas@live.com>
2026-04-03 09:16:21 +1300

    more tests

--- tests/SessionTests.cs
+++ tests/SessionTests.cs
@@ -14,7 +14,7 @@ public class SessionTests
     {
         fixture.GlobalSession.CreateSession(new SessionDesc
         {
-            Targets = [new TargetDesc { Format = SlangCompileTarget.Dxil }]
+            Targets = [new TargetDesc { Format = SlangCompileTarget.Spirv, Profile = fixture.GlobalSession.FindProfile("spirv_1_4") }]
         }, out var session).Succeeded.ShouldBeTrue();
 
         _fixture = fixture;
@@ -33,5 +33,51 @@ public class SessionTests
     public void EnsureSessionCreated()
     {
         _session.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void CompileAndCheckModule()
+    {
+        _session.GetLoadedModuleCount().ShouldBe(0);
+
+        var blob = Slang.CreateBlob("""
+            RWStructuredBuffer<int> outputBuffer;
+
+            public int foo(int a)
+            {
+                return a + 1;
+            }
+
+            [shader("compute")]
+            [numthreads(4, 1, 1)]
+            void computeMain(uint3 dispatchThreadID : SV_DispatchThreadID)
+            {
+                int index = (int)dispatchThreadID.x;
+                outputBuffer[index] = foo(index);
+            }
+            """u8);
+
+        var module = _session.LoadModuleFromSource("MyShader", string.Empty, blob, out _)!;
+        module.ShouldNotBeNull();
+
+        _session.GetLoadedModuleCount().ShouldBe(1);
+
+        module.GetTargetCode(0, out var spirv, out _).ShouldBe(SlangResult.SLANG_OK);
+        spirv.Buffer.Length.ShouldBeGreaterThan(0);
+
+        var mod0 = _session.GetLoadedModule(0);
+        mod0.ShouldBeSameAs(module);
+    }
+
+    [Fact]
+    public void CompileAndCheckModuleFromDisk()
+    {
+        _session.GetLoadedModuleCount().ShouldBe(0);
+
+        var module = _session.LoadModuleFromSource("MyShader", "Assets/MyShader.slang", null, out _)!;
+        module.ShouldNotBeNull();
+
+        module.GetTargetCode(0, out var spirv, out _).ShouldBe(SlangResult.SLANG_OK);
+        spirv.Buffer.Length.ShouldBeGreaterThan(0);
     }
 }
